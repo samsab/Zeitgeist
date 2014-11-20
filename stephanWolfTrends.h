91cf653f367451cd96ab7f8fc6a37063fa221b69
unknown <coopern3@EAS-BEN006-D5.it.muohio.edu>
2014-11-20 12:54:33 -0500

    Filled stephanWold.h with code

---
+++ stephanWolfTrends.h
@@ -0,0 +1,17 @@
+#include "Trends.h"
+
+#include <unordered_set>
+#include <vector>
+
+class stephanWolfTrends : public Trends {
+public:
+	//All of these should be the same
+	virtual void increaseCount(std::string s, unsigned int amount);
+	virtual unsigned int getCount(std::string s);
+	virtual std::string getNthPopular(unsigned int n);
+	virtual unsigned int numEntries();
+
+protected:
+	//Not sure what to do with this
+	std::vector<std::pair<int, std::string> > wordCountVector;
+};
