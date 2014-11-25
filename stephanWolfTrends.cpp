1bf5f29dd06c07d21271dd4ba12c0a2fbc7ef891
XCCOOP <country043@frontier.com>
2014-11-24 23:39:08 -0500

    Added working? getNth

--- stephanWolfTrends.cpp
+++ stephanWolfTrends.cpp
@@ -5,7 +5,7 @@ void stephanWolfTrends::increaseCount(std::string s, unsigned int amount) {
 
 	bool alreadyExists = false;
 
-	for(int i = 0; i < wordCountTable.size(); i++) {
+	for(unsigned int i = 0; i < wordCountTable.size(); i++) {
 		if(wordCountTable[i].first = s) {
 			wordCountTable[i].second + amount;
 			alreadyExists = true;
@@ -30,4 +30,14 @@ unsigned int stephanWolfTrends::getCount(std::string s){
 	}
 	//otherwise, return 0
 	return 0;
+}
+
+std::string stephanWolfTrends::getNthPopular(unsigned int n){
+	
+	if (n <= numEntries()){
+		return wordCountVector[n].second;
+	}
+
+	//If they give bad input, return empty string.
+	return "";
 }
