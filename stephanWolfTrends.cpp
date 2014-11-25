480feb00e116950e4df43bc6c7cd1a76b09639ca
XCCOOP <country043@frontier.com>
2014-11-24 23:05:39 -0500

    Added getCount

--- stephanWolfTrends.cpp
+++ stephanWolfTrends.cpp
@@ -11,7 +11,23 @@ void stephanWolfTrends::increaseCount(std::string s, unsigned int amount) {
 			alreadyExists = true;
 		} 
 	}
-	if(!alreadyExists) {
+	if(alreadyExists == false) {
 		wordCountTable.insert(s, 1);
 	}
+}
+
+unsigned int stephanWolfTrends::numEntries(){
+	return wordCountTable.size();
+}
+
+unsigned int stephanWolfTrends::getCount(std::string s){
+	//Check to see if word is present
+	for (unsigned int i = 0; i < wordCountTable.size(); i++){
+		if (wordCountTable[i].first == s){
+			//If so, return the count
+			return wordCountTable[i].second;
+		}
+	}
+	//otherwise, return 0
+	return 0;
 }
